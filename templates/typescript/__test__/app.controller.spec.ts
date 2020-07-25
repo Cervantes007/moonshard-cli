f172a049fef3992a811e5aa6a47b0e4537d438a5
cervantes007 <lmartellguevara@gmail.com>
2020-07-24 23:27:48 -0400

    fix(template): fix typescript template code style

--- templates/typescript/__test__/app.controller.spec.ts
+++ templates/typescript/__test__/app.controller.spec.ts
@@ -1,12 +1,21 @@
-import { inject } from 'moonshard'
+import { inject } from 'moonshard';
 
 describe('App Controller Test', () => {
   test('Hello World', async () => {
     const response = await inject({
       method: 'GET',
       url: `/`,
-    })
-    expect(response.statusCode).toBe(200)
-    expect(response.payload).toBe(`hello world!`)
-  })
-})
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.payload).toBe(`hello world!`);
+  });
+
+  test('Hi', async () => {
+    const response = await inject({
+      method: 'GET',
+      url: `/hi/John`,
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.payload).toBe(`MoonShard said: Hi, John!`);
+  });
+});
--- templates/typescript/__test__/jest.setup.ts
+++ templates/typescript/__test__/jest.setup.ts
@@ -1,9 +1,9 @@
-import { start, close } from 'moonshard'
+import { start, close } from 'moonshard';
 
 beforeAll(async () => {
-  await start()
-})
+  await start(9000, { logger: false });
+});
 
 afterAll(async () => {
-  await close()
-})
+  await close();
+});
